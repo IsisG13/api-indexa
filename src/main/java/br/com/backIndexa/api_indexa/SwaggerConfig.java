a92286c22383a2d8132e4f78d7ed3aaaeb854479
Marcelly Guimarães <souzaguimaraesarcelly@gmail.com>
2025-02-17 18:25:38 -0300

    conectando no front

---
+++ src/main/java/br/com/backIndexa/api_indexa/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package br.com.backIndexa.api_indexa;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("api-indexa-public")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API do Projeto Indexa")
+                        .version("1.0")
+                        .description("API para gerenciar o sistema de Indexa"));
+    }
+}
